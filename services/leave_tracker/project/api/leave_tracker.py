0e7522a50cc5045ec7c34ae1edca46da3a74b3be
Kevin Jose <kevin@Kevins-MacBook-Pro.local>
2019-04-17 09:44:30 +0530

    adding travis conf

---
+++ services/leave_tracker/project/api/leave_tracker.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify, request
+
+from project.api.utils import authenticate
+
+
+scores_blueprint = Blueprint('scores', __name__)
+
+
+@scores_blueprint.route('/scores/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
